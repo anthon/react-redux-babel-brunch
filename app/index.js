149c2907966e2bb8bcc9d6c16429351c9d462c37
Anthon Astrom <anthon@astromzimmer.com>
2016-05-11 14:24:39 +0200

    router

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Router, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import ReduxPromise from 'redux-promise'
-import App from './components/app'
+
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
-		<App/>
+		<Router history={browserHistory}/>
 	</Provider>
 ,document.getElementById('app'))
